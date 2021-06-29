96714a4f6c2493d8622158c53c3080230dfe548b
justinhui <wingchung@justinhui.co.uk>
2021-06-29 17:24:25 +0100

    Multipage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+import React, { useState } from "react";
 
 function App() {
+
+  const [pageNo, setPageNo] = useState(1); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {pageNo === 1 ? <Page1 /> : null }
+    {pageNo === 2 ? <Page2 /> : null }
+
+    <button onClick={() => setPageNo(2)}>Next Page</button>
     </div>
   );
 }
